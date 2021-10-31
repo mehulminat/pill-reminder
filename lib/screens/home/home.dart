6a6151b599fc0ee574af10fb7ccc123fa850026b
Mehul Minat <mehulminat321@gmail.com>
2021-11-01 00:51:12 +0530

    firstcommit

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,242 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:animated_widgets/animated_widgets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:pill_reminder/screens/add_new_medicine/add_new_medicine.dart';
+import '../../notifications/notifications.dart';
+import '../../database/repository.dart';
+import '../../models/pill.dart';
+import '../../screens/home/medicines_list.dart';
+import '../../screens/home/calendar.dart';
+import '../../models/calendar_day_model.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //-------------------| Flutter notifications |-------------------
+  final Notifications _notifications = Notifications();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  //===============================================================
+
+  //--------------------| List of Pills from database |----------------------
+  List<Pill> allListOfPills = List<Pill>();
+  final Repository _repository = Repository();
+  List<Pill> dailyPills = List<Pill>();
+  //=========================================================================
+
+  //-----------------| Calendar days |------------------
+  final CalendarDayModel _days = CalendarDayModel();
+  List<CalendarDayModel> _daysList;
+  //====================================================
+
+  //handle last choose day index in calendar
+  int _lastChooseDay = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initNotifies();
+    setData();
+    _daysList = _days.getCurrentDays();
+  }
+
+  //init notifications
+  Future initNotifies() async => flutterLocalNotificationsPlugin =
+      await _notifications.initNotifies(context);
+
+  //--------------------GET ALL DATA FROM DATABASE---------------------
+  Future setData() async {
+    allListOfPills.clear();
+    (await _repository.getAllData("Pills")).forEach((pillMap) {
+      allListOfPills.add(Pill().pillMapToObject(pillMap));
+    });
+    chooseDay(_daysList[_lastChooseDay]);
+  }
+  //===================================================================
+
+  @override
+  Widget build(BuildContext context) {
+    final double deviceHeight =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      extendBody: true,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          //refresh the pills from database
+          await Navigator.pushNamed(context, "/add_new_medicine")
+              .then((_) => setData());
+        },
+        tooltip: "Add Medicines",
+        child: Icon(Icons.ac_unit_outlined),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          color: Colors.blueAccent,
+          notchMargin: 6,
+          clipBehavior: Clip.antiAlias,
+          child: Padding(
+            padding: const EdgeInsets.all(2),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  padding: EdgeInsets.all(2.0),
+                  onPressed: () {
+                    print("calneder");
+                    // (Navigator.of(context).pushNamed('calendar'));
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (c, a1, a2) => AddNewMedicine(),
+                        // transitionDuration: Duration(milliseconds: 350),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          final tween = Tween(begin: begin, end: end);
+                          final offsetAnimation = animation.drive(tween);
+                          return SlideTransition(
+                            position: offsetAnimation,
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.calendar_today_rounded, color: Colors.white),
+                  iconSize: 30.0,
+                ),
+                SizedBox(
+                  width: 1.0,
+                ),
+                IconButton(
+                  padding: EdgeInsets.all(2.0),
+                  onPressed: () {
+                    print("person");
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (c, a1, a2) => AddNewMedicine(),
+                        // transitionDuration: Duration(milliseconds: 350),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(0.0, 1.0);
+                          const end = Offset.zero;
+                          final tween = Tween(begin: begin, end: end);
+                          final offsetAnimation = animation.drive(tween);
+                          return SlideTransition(
+                            position: offsetAnimation,
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.person, color: Colors.white),
+                  iconSize: 30.0,
+                ),
+              ],
+            ),
+          )),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: 0.0, left: 25.0, right: 25.0, bottom: 20.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: deviceHeight * 0.04,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    height: deviceHeight * 0.1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Journal",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline1
+                              .copyWith(color: Colors.black),
+                        ),
+                        ShakeAnimatedWidget(
+                          enabled: true,
+                          duration: Duration(milliseconds: 2000),
+                          curve: Curves.linear,
+                          shakeAngle: Rotation.deg(z: 30),
+                          child: Icon(
+                            Icons.notifications_none,
+                            size: 42.0,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: deviceHeight * 0.01,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Calendar(chooseDay, _daysList),
+                ),
+                SizedBox(height: deviceHeight * 0.03),
+                dailyPills.isEmpty
+                    ? SizedBox(
+                        width: double.infinity,
+                        height: 100,
+                        child: WavyAnimatedTextKit(
+                          textStyle: TextStyle(
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                          text: ["Loading..."],
+                          isRepeatingAnimation: true,
+                          speed: Duration(milliseconds: 150),
+                        ),
+                      )
+                    : MedicinesList(
+                        dailyPills, setData, flutterLocalNotificationsPlugin)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //-------------------------| Click on the calendar day |-------------------------
+
+  void chooseDay(CalendarDayModel clickedDay) {
+    setState(() {
+      _lastChooseDay = _daysList.indexOf(clickedDay);
+      _daysList.forEach((day) => day.isChecked = false);
+      CalendarDayModel chooseDay = _daysList[_daysList.indexOf(clickedDay)];
+      chooseDay.isChecked = true;
+      dailyPills.clear();
+      allListOfPills.forEach((pill) {
+        DateTime pillDate =
+            DateTime.fromMicrosecondsSinceEpoch(pill.time * 1000);
+        if (chooseDay.dayNumber == pillDate.day &&
+            chooseDay.month == pillDate.month &&
+            chooseDay.year == pillDate.year) {
+          dailyPills.add(pill);
+        }
+      });
+      dailyPills.sort((pill1, pill2) => pill1.time.compareTo(pill2.time));
+    });
+  }
+
+  //===============================================================================
+
+}
